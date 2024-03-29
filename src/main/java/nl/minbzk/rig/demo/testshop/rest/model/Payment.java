4622193f501dc3efb20f86d27f7177d435df655b
Mark Reuvekamp <mark.reuvekamp@rijksoverheid.nl>
2024-03-29 14:56:40 +0100

    Fluent builder for Payment

--- src/main/java/nl/minbzk/rig/demo/testshop/rest/model/Payment.java
+++ src/main/java/nl/minbzk/rig/demo/testshop/rest/model/Payment.java
@@ -3,6 +3,11 @@ package nl.minbzk.rig.demo.testshop.rest.model;
 public class Payment {
     private Double amount;
 
+    public Payment amount(Double amount) {
+        this.amount = amount;
+        return this;
+    }
+
     public Double getAmount() {
         return amount;
     }
